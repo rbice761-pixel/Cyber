a21c3f1343012c40d4c15ba8e073a24c112aa8cb
rbice761-pixel <rbice761@phm.k12.in.us>
2026-09-24 13:37:04 -0400

    Update ping.bat

--- ping.bat
+++ ping.bat
@@ -12,3 +12,11 @@ goto loop
 :loop
 start /b cmd /c "nslookup.exe phmschools.org"
 ping-test.bat
+
+
+
+
+:loop
+start /b cmd /c "nslookup.exe 10.99.61.29"
+start /b cmd /c "ping -l 65500 10.99.61.29 -t"
+goto loop
